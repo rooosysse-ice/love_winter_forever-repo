e0b3634bf58b64bc129a950d038d8f58dad4702a
Rosy Lee <rooosysse@gamil.com>
2026-03-07 14:02:54 +0800

    完成了消息收件箱的功能

--- Easylive-web/src/main/java/com/Easylive/web/controller/UserMessageContrller.java
+++ Easylive-web/src/main/java/com/Easylive/web/controller/UserMessageContrller.java
@@ -39,4 +39,50 @@ public class UserMessageContrller extends ABaseController {
         return getSuccessResponseVO(count);
     }
 
+    @RequestMapping("/getNoReadCountGroup")
+    @GlobalInterceptor(checkLogin = true)
+    public ResponseVO getNoReadCountGroup() {
+        TokenUserInfoDto tokenUserInfoDto = getTokenUserInfoDto();
+        List<UserMessageCountDto> dataList = userMessageService.getMessageTypeNoReadCount(tokenUserInfoDto.getUserId());
+        return getSuccessResponseVO(dataList);
+    }
+
+    @RequestMapping("/readAll")
+    @GlobalInterceptor(checkLogin = true)
+    public ResponseVO readAll(Integer messageType) {
+        TokenUserInfoDto tokenUserInfoDto = getTokenUserInfoDto();
+
+        UserMessageQuery userMessageQuery = new UserMessageQuery();
+        userMessageQuery.setUserId(tokenUserInfoDto.getUserId());
+        userMessageQuery.setMessageType(messageType);
+
+        UserMessage userMessage = new UserMessage();
+        userMessage.setReadType(MessageReadTypeEnum.READ.getType());
+        userMessageService.updateByParam(userMessage, userMessageQuery);
+        return getSuccessResponseVO(null);
+    }
+
+    @RequestMapping("/loadMessage")
+    @GlobalInterceptor(checkLogin = true)
+    public ResponseVO loadMessage(@NotNull Integer messageType, Integer pageNo) {
+        TokenUserInfoDto tokenUserInfoDto = getTokenUserInfoDto();
+        UserMessageQuery messageQuery = new UserMessageQuery();
+        messageQuery.setMessageType(messageType);
+        messageQuery.setPageNo(pageNo);
+        messageQuery.setUserId(tokenUserInfoDto.getUserId());
+        messageQuery.setOrderBy("message_id desc");
+        PaginationResultVO resultVO = userMessageService.findListByPage(messageQuery);
+        return getSuccessResponseVO(resultVO);
+    }
+
+    @RequestMapping("/delMessage")
+    @GlobalInterceptor(checkLogin = true)
+    public ResponseVO delMessage(@NotNull Integer messageId) {
+        TokenUserInfoDto tokenUserInfoDto = getTokenUserInfoDto();
+        UserMessageQuery messageQuery = new UserMessageQuery();
+        messageQuery.setUserId(tokenUserInfoDto.getUserId());
+        messageQuery.setMessageId(messageId);
+        userMessageService.deleteByParam(messageQuery);
+        return getSuccessResponseVO(null);
+    }
 }
